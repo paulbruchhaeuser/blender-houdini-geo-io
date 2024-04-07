d93bf1e3cb026335d0fad59b3d001b9715d412c6
Paul Florian Bruchhaeuser <paul.bruchhaeuser@gmail.com>
2024-04-07 17:11:42 +0200

    Frankensteined Version building with Hou 20 and Python 3.11 for Blender 4.11

--- hio/build.bat
+++ hio/build.bat
@@ -10,9 +10,9 @@ cd _build
 :: set cmake
 set CMAKE=H:/tools/00_develop_tools/cmake-3.24.0-rc4-windows-x86_64/bin/cmake.exe
 
-cmake .. -G "Visual Studio 16 2019" -DHFS=%1 -DPYTHON_EXECUTABLE=%2
-cmake --build . --config release
-::del CMakeCache.txt
+%CMAKE% .. -G "Visual Studio 17 2022" -DHFS=%1 -DPYTHON_EXECUTABLE=%2
+%CMAKE% --build . --config release
+del CMakeCache.txt
 
 cd ..
 ::rd /s /q _build
